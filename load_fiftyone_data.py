7ff3b0c9a4b5f3ab698d1f7f61b007e174b8acfa
John Paraskevopoulos <49733454+YiannisParask@users.noreply.github.com>
2024-08-09 18:57:34 +0300

    now successfully created tf dataset from fiftyone

--- load_fiftyone_data.py
+++ load_fiftyone_data.py
@@ -1,11 +1,16 @@
 import tensorflow as tf
 import fiftyone as fo
+import json
+import os
+from tensorflow.keras.utils import to_categorical
 
-
+# TODO: Handle case where labels are already in integer format
 class LoadFiftyoneDataset:
     ''' Class to load and preprocess Fiftyone data for training and inference. '''
-    def __init__(self, dataset_dir):
+    def __init__(self, dataset_dir, labels_dir):
         self.dataset_dir = dataset_dir
+        self.labels_dir = labels_dir
+        self.class_to_index = {}
 
     def load_fo_dataset(self, dataset_name):
         """Imports annotations into a FiftyOne dataset and returns it.
@@ -21,17 +26,50 @@ class LoadFiftyoneDataset:
             │   ├── image_2.png
             │   └── ...
         """
-        # Check if the dataset name exists, load it if it does
         if dataset_name in fo.list_datasets():
             dataset = fo.load_dataset(dataset_name)
         else:
-            # Create a new FiftyOne dataset if it doesn't exist
             dataset = fo.Dataset.from_dir(
                 dataset_dir=self.dataset_dir,
                 dataset_type=fo.types.ImageClassificationDirectoryTree,
                 name=dataset_name,
             )
         return dataset
+    
+    def load_labels_from_json(self):
+        '''Load labels from a JSON file'''
+        with open(self.labels_dir, "r") as file:
+            labels_data = json.load(file)
+        label_mappings = labels_data["labels"] 
+        return label_mappings
+    
+    def load_labels_from_fiftyone(self, file_paths, label_mappings):
+        '''Load labels from a FiftyOne dataset'''
+        labels = []
+        missing_labels = []
+        for fp in file_paths:
+            filename = os.path.splitext(os.path.basename(fp))[0]
+            if filename in label_mappings:
+                labels.append(label_mappings[filename][0])
+            else:
+                missing_labels.append(filename)
+                labels.append("unknown")
+
+        if missing_labels:
+            print(f"Missing labels for the following files: {missing_labels}")
+
+        # Convert categorical labels to indices
+        unique_labels = set(labels)
+        self.class_to_index = {label: idx for idx, label in enumerate(unique_labels)}
+
+        # Convert labels to integer indices
+        labels = [self.class_to_index[label] for label in labels]
+        
+        # Filter out samples with missing labels
+        valid_indices = [i for i, label in enumerate(labels) if label != -1]
+        file_paths = [file_paths[i] for i in valid_indices]
+        labels = [labels[i] for i in valid_indices]
+        return file_paths, labels
 
     def load_and_preprocess_image(self, file_path, image_size=(224, 224), is_rgb=False):
         """Loads and preprocesses an image for inference using TensorFlow."""
@@ -40,27 +78,24 @@ class LoadFiftyoneDataset:
         image = tf.image.resize(image, image_size)
         if not is_rgb:
             image = tf.image.rgb_to_grayscale(image)
-        image = tf.cast(image, tf.float32) / 255.0
+        image = image / 255.0
         return image
 
-    def fiftyone_to_tf_dataset(self, fo_dataset_view, image_size=(224, 224), batch_size=32, is_rgb=False):
-        """Converts a FiftyOne dataset view to a TensorFlow Dataset (tf.data.Dataset)."""
-        file_paths = fo_dataset_view.values("filepath")
-        labels = fo_dataset_view.values("ground_truth")
-        file_paths = [str(fp) for fp in file_paths]
+    def fiftyone_to_tf_dataset(self, fo_dataset, image_size=(224, 224), batch_size=32, is_rgb=False):
+        file_paths = fo_dataset.values("filepath")
+        label_mappings = self.load_labels_from_json()
+        file_paths, labels = self.load_labels_from_fiftyone(file_paths, label_mappings) 
+        num_classes = len(self.class_to_index)
+        labels = to_categorical(labels, num_classes=num_classes)
 
         def load_image_and_label(file_path, label):
-            image = self.load_and_preprocess_image(file_path.numpy().decode(), image_size, is_rgb)
+            image = self.load_and_preprocess_image(file_path, image_size, is_rgb)
             return image, label
 
         tf_dataset = tf.data.Dataset.from_tensor_slices((file_paths, labels))
         tf_dataset = tf_dataset.map(
-            lambda file_path, label: tf.py_function(
-                func=load_image_and_label,
-                inp=[file_path, label],
-                Tout=[tf.float32, tf.string],
-            ),
-            num_parallel_calls=tf.data.AUTOTUNE,
+            lambda file_path, label: load_image_and_label(file_path, label),  
+            num_parallel_calls=tf.data.AUTOTUNE
         )
         tf_dataset = tf_dataset.batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
         return tf_dataset
