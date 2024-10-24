7ee3f281ed9724bcf3d9dbfabb899aa4fb6dbda4
John Paraskevopoulos <49733454+YiannisParask@users.noreply.github.com>
2024-10-24 22:41:04 +0300

    minor corrections

--- load_fiftyone_data.py
+++ load_fiftyone_data.py
@@ -4,6 +4,7 @@ import json
 import os
 from tensorflow.keras.utils import to_categorical
 import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
 
 # TODO: Handle case where labels are already in integer format
 class LoadFiftyoneDataset:
@@ -30,15 +31,17 @@ class LoadFiftyoneDataset:
                 name=dataset_name,
             )
         return dataset
-    
-    def _load_labels_from_json(self):
+
+
+    def load_labels_from_json(self):
         '''Load labels from a JSON file'''
         with open(self.labels_dir, "r") as file:
             labels_data = json.load(file)
-        label_mappings = labels_data["labels"] 
+        label_mappings = labels_data["labels"]
         return label_mappings
-    
-    def _load_labels_from_fiftyone(self, file_paths, label_mappings):
+
+
+    def load_labels_from_fiftyone(self, file_paths, label_mappings):
         '''
         Load labels from Fiftyone dataset
         Args:
@@ -55,22 +58,23 @@ class LoadFiftyoneDataset:
         for fp in file_paths:
             filename = os.path.splitext(os.path.basename(fp))[0]
             if filename in label_mappings:
-                labels.append(label_mappings[filename][0])
+                labels.append(label_mappings[filename])
             else:
                 missing_labels.append(filename)
-                labels.append("unknown")
+                labels.append([])
 
         if missing_labels:
             print(f"Missing labels for the following files: {missing_labels}")
-        
+
         # Filter out samples with missing labels (if exist)
-        valid_indices = [i for i, label in enumerate(labels) if label != "unknown"]
+        valid_indices = [i for i, label in enumerate(labels) if len(label) > 0]
         file_paths = [file_paths[i] for i in valid_indices]
         labels = [labels[i] for i in valid_indices]
+
         return file_paths, labels
 
-    def _load_and_preprocess_image(self, file_path, image_size=(224, 224), is_rgb=False):
-        ''' 
+    def load_image_and_label(self, file_path, label, image_size=(224, 224), is_rgb=False):
+        '''
         Load and preprocess image.
         Args:
             file_path: String
@@ -85,11 +89,12 @@ class LoadFiftyoneDataset:
         if not is_rgb:
             image = tf.image.rgb_to_grayscale(image)
         image = image / 255.0
-        return image
+        return image, label
+
 
     def fiftyone_to_tf_dataset(self, fo_dataset, image_size=(224, 224), batch_size=32, is_rgb=False):
         '''
-        Convert Fiftyone dataset to TensorFlow dataset        
+        Convert Fiftyone dataset to TensorFlow dataset
         Args:
             fo_dataset: Fiftyone dataset
             image_size: Tuple of image dimensions
@@ -99,24 +104,24 @@ class LoadFiftyoneDataset:
             tf_dataset: TensorFlow dataset
         '''
         file_paths = fo_dataset.values("filepath")
-        label_mappings = self._load_labels_from_json()
-        file_paths, labels = self._load_labels_from_fiftyone(file_paths, label_mappings)
-        
-        labels = to_categorical(labels)
+        label_mappings = self.load_labels_from_json()
+        file_paths, labels = self.load_labels_from_fiftyone(file_paths, label_mappings)
 
-        def _load_image_and_label(file_path, label):
-            image = self._load_and_preprocess_image(file_path, image_size, is_rgb)
-            return image, label
+        labels = to_categorical(labels)
+        # mlb = MultiLabelBinarizer()
+        # labels = mlb.fit_transform(labels)
 
         tf_dataset = tf.data.Dataset.from_tensor_slices((file_paths, labels))
         tf_dataset = tf_dataset.map(
-            lambda file_path, label: _load_image_and_label(file_path, label),  
+            lambda file_path, label: self.load_image_and_label(file_path, label, image_size, is_rgb),
             num_parallel_calls=tf.data.AUTOTUNE
         )
         tf_dataset = tf_dataset.batch(batch_size).prefetch(buffer_size=tf.data.AUTOTUNE)
+
         return tf_dataset
-    
-    def get_samples_labels(self, train, test):
+
+
+    def get_samples_labels(self, train, test, valid=None):
         '''
         Get samples and labels from TensorFlow datasets
         Args:
@@ -129,19 +134,27 @@ class LoadFiftyoneDataset:
         # extract the images and labels for each dataset
         train_images = np.concatenate(list(train.map(lambda x, y: x)))
         train_labels = np.concatenate(list(train.map(lambda x, y: y)))
-        
+
         test_images = np.concatenate(list(test.map(lambda x, y: x)))
         test_labels = np.concatenate(list(test.map(lambda x, y: y)))
-        
+
+        if valid is not None:
+            valid_images = np.concatenate(list(valid.map(lambda x, y: x)))
+            valid_labels = np.concatenate(list(valid.map(lambda x, y: y)))
+            inputs = np.concatenate((train_images, valid_images, test_images), axis = 0)
+            targets = np.concatenate((train_labels, valid_labels, test_labels), axis = 0)
+            return inputs, targets
+
         # join them together
         inputs = np.concatenate((train_images, test_images), axis = 0)
         targets = np.concatenate((train_labels, test_labels), axis = 0)
-        
+
         return inputs, targets
 
+
     def splits(self, dataset, TRAIN_RATIO=0.7, VAL_RATIO=0.2, TEST_RATIO=0.1):
-        ''' 
-        Split dataset into training, validation, and test sets. 
+        '''
+        Split dataset into training, validation, and test sets.
         Args:
             dataset: TensorFlow dataset
             TRAIN_RATIO: Float
@@ -157,7 +170,7 @@ class LoadFiftyoneDataset:
         train_dataset = dataset.take(int(TRAIN_RATIO*DATASET_SIZE))
 
         val_test_dataset = dataset.skip(int(TRAIN_RATIO*DATASET_SIZE))
-        val_dataset = val_test_dataset.take(int(VAL_RATIO*DATASET_SIZE))  
+        val_dataset = val_test_dataset.take(int(VAL_RATIO*DATASET_SIZE))
 
         test_dataset = val_test_dataset.skip(int(TEST_RATIO*DATASET_SIZE))
         return train_dataset, val_dataset, test_dataset
